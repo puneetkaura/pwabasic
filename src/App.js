9d4c46c5c0a326f36d45c6cf02af345807e6ee80
Tahir Khan <tahirdecoy786@gmail.com>
2024-04-17 10:53:15 +0530

    add index and minor css

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ function App() {
     <div className="App">
       <div class="h-full w-full bg-teal-400 w-1/2 p-4 mx-auto text-center border">
      
-        <><h1 class="text-2xl">Mrs. Anjali chikkara PREGNANCY TRACKER</h1>
+        <><h1 class="text-4xl text-center text-pink-700	 ">Mrs. Anjali chikkara PREGNANCY TRACKER</h1><br></br>
         <><h2 class="text-2xl"> Full Name : {name} </h2><h2 class="text-2xl"> lmpDate : {lmpDate} </h2></></>
          
     </div>
     <table>
-      <thead class="h-full w-full bg-teal-400 w-1/2 p-4 mx-auto text-center border">
-        <tr>
+      <thead class="h-full w-full bg-teal-400 w-1/2 p-4 mx-auto text-center text-pink-700 border">
+        <tr class="bg-white">
              <th>Id</th>
               <th>Checkup</th>
               <th>Importance</th>
@@ -34,9 +34,10 @@ function App() {
      
       <tbody>
        {
+        // DATA.filter(item => item.StartDate > lmpDate).map((item)=>
         DATA.map((item)=>(
-        <tr>
-          <td>{item["Id"]}</td>
+        <tr class="even:bg-gray-100 odd:bg-white">
+        <td className='text-pink-700 text-3xl'>{item["Id"]}</td>
         <td>{item["Checkup"]}</td>
         <td>{item["Importance"]}</td>
         <td>{item["Trimsester"]}</td>
