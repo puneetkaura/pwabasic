d9c50c068720b0930379d23774f685567c1f987b
Tahir Khan <tahirdecoy786@gmail.com>
2024-04-20 09:53:47 +0530

    add accordian

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import './App.css';
 import DATA from './data';
-
+import React, { useState } from 'react';
+import Accordion from './accordian'
 import { useSearchParams } from "react-router-dom";
-
-
 
 function App() {
   const [searchParams] = useSearchParams();
@@ -12,51 +11,49 @@ function App() {
   return (
     <div className="App">
       <div class="h-full w-full bg-blue-500 w-1/2 p-2 mx-auto text-center border">
-     
-        <><h1 class="text-4xl text-center text-white 	height: 1px;">Pregnancy Tests Chart By Dr Richa Malhan</h1><br/>
-       
-        <div className='	flex-direction: column-reverse;'><h2 class="text-2xl">
-          <span className='text-white text-2xl'> Patient Name : {name}   LMP Date : {lmpDate} </span> </h2> </div>
+
+        <><h1 class="text-2xl text-center text-white ">Pregnancy Tests Chart By Dr Richa Malhan</h1>
+
+          <div className='	flex-direction: column-reverse;'><h2 class="text-1xl">
+            <span className='text-white text-1xl'> Patient Name : {name}   LMP Date : {lmpDate} </span> </h2> </div>
         </>
-       
-         
-    </div>
-    <table>
-      <thead class="h-full w-full bg-teal-400 w-1/2 p-4 mx-auto text-center text-pink-700 border">
-        <tr class="bg-white">
-             <th>Id</th>
-              <th>Checkup</th>
-              <th>Importance</th>
-              <th>Trimsester</th>
-              <th>Pregnancy month</th>
-              <th>Start week</th>
-              <th>Week due</th>
-              <th>Start Date</th>
-              <th>Due Date</th>
-            </tr>
-      </thead>
-     
-      <tbody>
-       {
-       
-      //  DATA.filter(item => item.StartDate > lmpDate).map((item)=>(
-        DATA.map((item)=>(
-        <tr class="even:bg-gray-100 odd:bg-white">
-        <td className='text-pink-700 text-3xl'>{item["Id"]}</td>
-        <td>{item["Checkup"]}</td>
-        <td>{item["Importance"]}</td>
-        <td>{item["Trimsester"]}</td>
-        <td>{item["Pregnancymonth"]}</td>
-        <td>{item["Startweek"]}</td>
-        <td>{item["Weekdue"]}</td>
-        <td>{item["StartDate"]}</td>
-        <td>{item["DueDate "]}</td>
-      </tr>
-      ))}
-       
-      </tbody>
+      </div>
+      <table>
+        <thead class="h-full w-full bg-teal-400 w-1/2 p-4 mx-auto text-center text-pink-700 border">
+          <tr class="bg-white">
+            <th>Importance</th>
+            <th>Checkup</th>
+            <th>Trimsester</th>
+            <th>Pregnancy month</th>
+            <th>Start week</th>
+            <th>Week due</th>
+            <th>Start Date</th>
+            <th>Due Date</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {
+            //  DATA.filter(item => item.StartDate > lmpDate).map((item)=>(
+            DATA.map((item) => (
+
+              <tr className="even:bg-gray-100 odd:bg-white accordion">
+                  
+                <td className='text-pink-700 text-1xl'>
+                <Accordion  Importance={item["Importance"]} /></td>
+                <td>{item["Checkup"]}</td>
+                <td>{item["Trimsester"]}</td>
+                <td>{item["Pregnancymonth"]}</td>
+                <td>{item["Startweek"]}</td>
+                <td>{item["Weekdue"]}</td>
+                <td>{item["StartDate"]}</td>
+                <td>{item["DueDate "]}</td>
+              </tr>
+            ))}
+
+        </tbody>
       </table>
-  
+
     </div>
   );
 }
---
+++ src/accordian.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const Accordion = ({Importance }) => {
+    const [isActive, setIsActive] = useState(false);
+  
+    return (
+      <div className="accordion-item">
+        <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
+          {/* <div>{Id}</div> */}
+          <div className='text-3xl'>{isActive ? '-' : '+'}</div>
+        </div>
+        {isActive && <div className="accordion-content">{Importance}</div>}
+      </div>
+    );
+  };
+  
+  export default Accordion;
