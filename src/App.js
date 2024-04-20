3f9847843fbb9b4429aed0cc42852c6816b18482
Tahir Khan <tahirdecoy786@gmail.com>
2024-04-20 18:28:22 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import DATA from './data';
 import Accordion from './accordian'
 import { useSearchParams } from "react-router-dom";
 
+
 function App() {
   const [searchParams] = useSearchParams();
   const name = searchParams.get("name")
@@ -10,9 +11,7 @@ function App() {
   return (
     <div className="App">
       <div class="h-full w-full bg-blue-500 w-1/2 p-2 mx-auto text-center border">
-
-        <><h1 class="text-2xl text-center text-white ">Pregnancy Tests Chart By Dr Richa Malhan</h1>
-
+        <><h1 class="text-1xl text-center text-white ">Pregnancy Tests Chart By Dr Richa Malhan</h1>
           <div className='	flex-direction: column-reverse;'><h2 class="text-1xl">
             <span className='text-white text-1xl'> Patient Name : {name}   LMP Date : {lmpDate} </span> </h2> </div>
         </>
@@ -20,7 +19,7 @@ function App() {
       <table>
         <thead class="h-full w-full bg-teal-400 w-1/2 p-4 mx-auto text-center text-pink-700 border">
           <tr class="bg-white">
-            {/* <th>Importance</th> */}
+            <th>Id</th>
             <th>Checkup</th>
             <th>Trimsester</th>
             <th>Pregnancy month</th>
@@ -28,17 +27,18 @@ function App() {
             <th>Due Date</th>
           </tr>
         </thead>
-
         <tbody>
           {
             //  DATA.filter(item => item.StartDate > lmpDate).map((item)=>(
             DATA.map((item) => (
 
               <tr className="even:bg-gray-100 odd:bg-white accordion">
-                <td className='text-pink-700 text-1xl'>
+                <td className='text-pink-700 text-3xl'>
+                {item["Id"]}
                 </td>
                 <td>{item["Checkup"]}
-                <Accordion  Importance={item["Importance"]} />
+                <Accordion Importance={item["Importance"]} />
+                
                 </td>
                 <td>{item["Trimsester"]}</td>
                 <td>{item["Pregnancymonth"]}</td>
@@ -46,7 +46,6 @@ function App() {
                 <td>{item["DueDate"]}</td>
               </tr>
             ))}
-
         </tbody>
       </table>
 
--- src/accordian.jsx
+++ src/accordian.jsx
@@ -6,8 +6,7 @@ const Accordion = ({Importance }) => {
     return (
       <div className="accordion-item">
         <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
-          {/* <div>{Id}</div> */}
-          <div className='text-1xl'>{isActive ? '-' : '+'}</div>
+          <div className='text-1xl'>{isActive ? <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" id="up-arrow"><path fill="#000" fill-rule="evenodd" d="M5.306 15.694a1.043 1.043 0 0 0 1.476 0L12 10.47l5.218 5.224a1.043 1.043 0 0 0 1.476 0 1.046 1.046 0 0 0 0-1.478l-5.904-5.91a1.04 1.04 0 0 0-.79-.305 1.04 1.04 0 0 0-.79.305l-5.904 5.91a1.046 1.046 0 0 0 0 1.478Z" clip-rule="evenodd"></path></svg> : <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" id="down-arrow"><path d="M22.782 13.8 17 19.582 11.218 13.8a1 1 0 0 0-1.414 1.414L16.29 21.7a.992.992 0 0 0 .71.292.997.997 0 0 0 .71-.292l6.486-6.486a1 1 0 0 0-1.414-1.414z"></path></svg>}</div>
         </div>
         {isActive && <div className="accordion-content">{Importance}</div>}
       </div>
